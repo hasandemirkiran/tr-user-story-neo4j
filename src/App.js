22d3a450942ddc9da571b03a44d0a4b354e30c52
Hasan Demirkıran <hasan.demirkiran@westerops.com>
2021-06-25 11:31:04 +0300

    Home page design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appWrapper">
+      <h1 className='header1'>Turkish User Story Graph Data Base Creator</h1>
+      <div class="input-group mb-3">
+      <input type="file" class="form-control" id="inputGroupFile02"/>
+      <label class="input-group-text" for="inputGroupFile02">Upload User Story File</label>
+    </div>
     </div>
   );
 }
