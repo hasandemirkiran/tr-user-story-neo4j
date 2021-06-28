7876478b244ce126cee5a58170ca58b3b303ba3f
Hasan Demirkıran <hasan.demirkiran@westerops.com>
2021-06-28 12:33:25 +0300

    File Upload Component Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.scss';
+import "./App.scss";
+import FileUpload from "./FileUpload";
 
 function App() {
   return (
     <div className="appWrapper">
-      <h1 className='header1'>Turkish User Story Graph Data Base Creator</h1>
-      <div class="input-group mb-3">
-      <input type="file" class="form-control" id="inputGroupFile02"/>
-      <label class="input-group-text" for="inputGroupFile02">Upload User Story File</label>
-    </div>
+      <h1 className="header1">Turkish User Story Graph Data Base Creator</h1>
+      {/* <div class="input-group mb-3">
+        <input type="file" class="form-control" id="inputGroupFile" />
+        <label class="input-group-text" for="inputGroupFile02">
+          Upload User Story File
+        </label>
+      </div> */}
+      <FileUpload />
     </div>
   );
 }
