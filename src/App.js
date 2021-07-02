356c09755141ef8deb735008acb504122c1c99db
Hasan Demirkıran <hasan.demirkiran@westerops.com>
2021-07-02 09:43:06 +0300

    app.js added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ function App() {
   return (
     <div className="appWrapper">
       <h1 className="header1">Turkish User Story Graph Data Base Creator</h1>
-      {/* <div class="input-group mb-3">
-        <input type="file" class="form-control" id="inputGroupFile" />
-        <label class="input-group-text" for="inputGroupFile02">
-          Upload User Story File
-        </label>
-      </div> */}
       <FileUpload />
     </div>
   );
